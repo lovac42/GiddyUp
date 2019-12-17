f38c2ac696fd0d840f1f33825b8d5628f50346f1
lovac42 <lovac42@protonmail.com>
2019-12-17 02:16:35 -0500

    added CR

--- src/giddy_up/main.py
+++ src/giddy_up/main.py
@@ -1,12 +1,13 @@
-
+# -*- coding: utf-8 -*-
+# Copyright 2019 Lovac42
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+# Support: https://github.com/lovac42/GiddyUp
 
 
 from aqt.utils import askUser
 from anki.utils import intTime
 from anki.hooks import wrap
 from anki.lang import _
-
-
 
 
 
--- src/giddy_up_addon.py
+++ src/giddy_up_addon.py
@@ -1,1 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Lovac42
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+# Support: https://github.com/lovac42/GiddyUp
+
+
 import giddy_up
