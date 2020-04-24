37a4de001b635b5c7fd9ffa653d8bae66085f1e4
lovac42 <lovac42@protonmail.com>
2020-04-23 22:29:53 -0400

    bump version to v0.1.0

--- src/build.bat
+++ src/build.bat
@@ -1,18 +1,24 @@
 @echo off
 set ZIP=C:\PROGRA~1\7-Zip\7z.exe a -tzip -y -r
 set REPO=giddy_up
-set NAME=giddy_up
-set PACKID=GiddyUp
-set VERSION=0.0.1
+set NAME=GiddyUp
+set PACKID=giddy_up
+set VERSION=0.1.0
 
 
-quick_manifest.exe "%NAME%" "%PACKID%" >%REPO%\manifest.json
 echo %VERSION% >%REPO%\VERSION
 
 fsum -r -jm -md5 -d%REPO% * > checksum.md5
 move checksum.md5 %REPO%\checksum.md5
 
+
+
 %ZIP% %NAME%_v%VERSION%_Anki20.zip *.py %REPO%\*
+
 cd %REPO%
+
+quick_manifest.exe "%NAME%" "%PACKID%" >manifest.json
 %ZIP% ../%NAME%_v%VERSION%_Anki21.ankiaddon *
+
+quick_manifest.exe "%NAME%" "%PACKID%" >manifest.json
 %ZIP% ../%NAME%_v%VERSION%_CCBC.adze *
